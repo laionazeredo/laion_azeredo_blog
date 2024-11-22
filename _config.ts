a799e071bf1b1750d5cbc74ae028d203ea31914f
laionazeredo <laion.azeredo@gmail.com>
2024-11-22 18:55:00 -0300

    feat: add first blog post

--- _config.ts
+++ _config.ts
@@ -1,8 +1,10 @@
-import lume from "lume/mod.ts";
+import basePath from "lume/plugins/base_path.ts";
 import blog from "blog/mod.ts";
+import lume from "lume/mod.ts";
 
 const site = lume();
 
 site.use(blog());
+site.use(basePath());
 
 export default site;
